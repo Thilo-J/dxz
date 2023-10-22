f8e8ac3026a31bab5bb24e676855e7342ef043b4
Thilo Langensteiner <thilo.j.la@gmail.com>
2023-10-22 15:31:59 +0200

    init commit

--- my_dxz.h
+++
@@ -1,4 +0,0 @@
-#include <vector>
-#include <list>
-
-std::list<std::vector<int>>  dxz_get_exact_covers(int rows, int cols, std::list<int> matrix);
--- src/dxz.cpp
+++ src/dxz.cpp
@@ -8,7 +8,6 @@ extern "C" {
 #include <tuple>
 #include <list>
 #include <sstream>
-#include "my_dxz.h"
 #include <vector>
 #include <algorithm>
 
@@ -35,9 +34,22 @@ struct Node {
     const Node* right;
 };
 
-
 const Node* top = new Node({-1, NULL, NULL});
 const Node* bottom = new Node({-2, NULL, NULL});
+
+const Node* get_node(int value, const Node* left, const Node* right)
+{
+    if (right->value == bottom->value)
+        return left;
+    
+    return  new Node({
+            value,
+            left,
+            right
+    });
+    
+}
+
 
 struct dxz_manager {
     const Node* root = bottom;
@@ -78,12 +90,8 @@ const Node* search(list sparse_matrix, int k, int max, dxz_manager* manager) {
         const Node* y = search(sparse_matrix, k+1, max, manager);        
         for (next = row; (next = get_left(next)) != row; )
             uncover_column(get_data(next)->list_data);
-        if (y->value != -2) {
-            x = new Node({
-                row_index,
-                x,
-                y
-            });
+        if (y->value != bottom->value) {
+            x = get_node(row_index, x, y);
             node_counter++;
             manager->root = x;            
         }
@@ -96,41 +104,75 @@ const Node* search(list sparse_matrix, int k, int max, dxz_manager* manager) {
 }
 
 
-void preorder_traversal(const Node* node, std::vector<int>* solution, std::list<std::vector<int>>* solutions)
+void preorder_traversal(const Node* node, std::vector<int>* solution, std::list<std::vector<int>>* solutions, int max_solutions)
 {
-    if(node->value == -2)
+    if(max_solutions != 0 && solutions->size() >= max_solutions)
         return;
-    if(node->value == -1)
+    if(node->value == bottom->value)
+        return;
+    if(node->value == top->value)
         return solutions->push_back(*solution);
-    preorder_traversal(node->left, solution, solutions);
+    preorder_traversal(node->left, solution, solutions, max_solutions);
     solution->push_back(node->value);
-    preorder_traversal(node->right, solution, solutions);
+    preorder_traversal(node->right, solution, solutions, max_solutions);
     solution->pop_back();
 }
 
 
-std::list<std::vector<int>> zdd_to_list(const Node* root)
+std::list<std::vector<int>> zdd_to_list(const Node* root, int max_solutions)
 {
     std::list<std::vector<int>> solutions;
     std::vector<int> solution;
-    preorder_traversal(root, &solution, &solutions);
+    preorder_traversal(root, &solution, &solutions, max_solutions);
     return solutions;
 }
 
-std::list<std::vector<int>>  dxz_get_exact_covers(int rows, int cols, std::list<int> matrix) {
+std::list<std::vector<int>>  dxz_get_exact_covers(int rows, int cols, std::list<int> matrix, int max_solutions) {
     int arr[matrix.size()];
     std::copy(matrix.begin(), matrix.end(), arr);
     dxz_manager manager;
     list sparse_matrix;
     sparse_matrix = create_sparse(rows, cols, arr);
     search(sparse_matrix, 0, rows, &manager);
-    std::list<std::vector<int>>  solutions = zdd_to_list(manager.root);
+    std::list<std::vector<int>>  solutions = zdd_to_list(manager.root, max_solutions);
     return solutions;
+}
+
+int node_count(const Node* node)
+{
+    if (node->value == bottom->value) return 0; 
+    if (node->value == top->value)  return 1;
+    return node_count(node->left) + node_count(node->right);
+}
+
+
+int dxz_get_number_of_solutions(int rows, int cols, std::list<int> matrix) {
+    int arr[matrix.size()];
+    std::copy(matrix.begin(), matrix.end(), arr);
+    dxz_manager manager;
+    list sparse_matrix;
+    sparse_matrix = create_sparse(rows, cols, arr);
+    search(sparse_matrix, 0, rows, &manager);
+    return node_count(manager.root);
 }
 
 PYBIND11_MODULE(dxz, module_handle)
 {
     module_handle.doc() = "Exact cover solver that returns all solutions";
-    module_handle.def("dxz_solve", &dxz_get_exact_covers);
+    module_handle.def("dxz_solve", &dxz_get_exact_covers, 
+    "Returns all solutions to a given exact cover problem.\n\
+    Paramaters:\n\
+        arg0: number of rows\n\
+        arg1: number of columns\n\
+        arg2: matrix in a single list\n\
+        arg3: max number of solutions to be returned (0 for all)"
+    );
+    module_handle.def("dxz_count", &dxz_get_number_of_solutions, 
+    "Returns the number of solutions of a given exact cover problem.\n\
+    Paramaters:\n\
+        arg0: number of rows\n\
+        arg1: number of columns\n\
+        arg2: matrix in a single list"
+    );
 }
 
